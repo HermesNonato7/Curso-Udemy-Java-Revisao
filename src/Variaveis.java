ee72845bc0213504aabebe9c817f11ec49ada7a8
Hermes Nonato <99772475+hermesnonato7@users.noreply.github.com>
2024-08-05 21:04:53 -0300

    Update Variaveis.java

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -5,7 +5,7 @@ public class Variaveis {
     public static void main(String[] args) {
         int idade = 38;
 
-        System.out.println(idade);
+        System.out.println(idade + 2);
     }
 
 }

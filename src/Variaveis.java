fb6a5636baeba307d3da70142c08d2e83de1325f
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-06 22:35:29 -0300

    commit teste 3

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -5,7 +5,7 @@ public class Variaveis {
     public static void main(String[] args) {
         int idade = 38;
 
-        System.out.println(idade + 3);
+        System.out.println("Minha idade é: " + (idade  + 3) +" anos");
     }
 
 }

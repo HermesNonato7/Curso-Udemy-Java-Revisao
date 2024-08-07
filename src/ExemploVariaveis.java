963824672fd389a42daef9ed6850baeb696c8fce
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-07 08:19:52 -0300

    add exemplo constante

--- src/ExemploVariaveis.java
+++ src/ExemploVariaveis.java
@@ -1,19 +1,16 @@
-import com.sun.security.jgss.GSSUtil;
-import org.w3c.dom.ls.LSOutput;
-
 public class ExemploVariaveis {
 
     public static void main(String[] args) {
         int idade = 25;
         double altura = 175;
         char genero = 'M';
-        boolean estaChovendo = true;
+        boolean clienteAtivo = true;
         String nome = "João";
 
         System.out.println("Idade: " + idade);
         System.out.println("Altura: " + altura);
         System.out.println("Gênero: " + genero);
-        System.out.println("Está chovendo? " + estaChovendo);
+        System.out.println("Está chovendo? " + clienteAtivo);
         System.out.println("Nome: " + nome);
 
     }

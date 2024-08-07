dc392ee04fe2668dd72d1f72c04640840a3bc8c0
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-07 08:07:26 -0300

    add exemplo constante

---
+++ src/ExemploVariaveis.java
@@ -0,0 +1,20 @@
+import com.sun.security.jgss.GSSUtil;
+import org.w3c.dom.ls.LSOutput;
+
+public class ExemploVariaveis {
+
+    public static void main(String[] args) {
+        int idade = 25;
+        double altura = 175;
+        char genero = 'M';
+        boolean estaChovendo = true;
+        String nome = "João";
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(genero);
+        System.out.println(estaChovendo);
+        System.out.println(nome);
+
+    }
+}

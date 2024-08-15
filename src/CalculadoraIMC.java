ec3a9481187e2c847a119c55754ee189358462ee
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-14 21:55:32 -0300

    add calculadora de imc

---
+++ src/CalculadoraIMC.java
@@ -0,0 +1,12 @@
+public class CalculadoraIMC {
+
+    public static void main(String[] args) {
+        // peso div (altura * altura)
+
+        double peso = 1.80;
+        double altura = 1.75;
+
+        System.out.println(peso / (altura * altura));
+    }
+
+}

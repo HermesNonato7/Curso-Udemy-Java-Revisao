11e5149f84cc539f3a68d05459efd94d508ad83c
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-14 22:40:17 -0300

    scanner na calculadora de imc

--- src/CalculadoraIMC.java
+++ src/CalculadoraIMC.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class CalculadoraIMC {
 
     public static void main(String[] args) {
-        // peso div (altura * altura)
 
-        float peso = 1.80f;
-        float altura = 1.75f;
+        Scanner leitorTeclado = new Scanner(System.in);
+
+        // peso div (altura * altura)
+        System.out.println("Digite o seu peso:");
+        float peso = leitorTeclado.nextFloat();
+        System.out.println("Agora digitea sua altura em cm:");
+        float altura = leitorTeclado.nextFloat();
 
         float resultado = peso / (altura * altura);
 
         System.out.println("O IMC para o peso " + peso +
                 " e altura " + altura + " é " + resultado);
+
+        leitorTeclado.close();
 
     }
 }

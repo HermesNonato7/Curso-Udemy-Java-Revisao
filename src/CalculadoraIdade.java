cd7baf9898466046358281dfd9f35770f6a14f53
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-14 22:42:35 -0300

    scanner na calculadora de idade

--- src/CalculadoraIdade.java
+++ src/CalculadoraIdade.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CalculadoraIdade {
 
     public static void main(String[] args) {
@@ -5,12 +7,16 @@ public class CalculadoraIdade {
         // 2 -> pegar o anode nascimento
         // 3 -> subtrair o ano atual - ano de nascimento
 
-        int anoAtual = 2024;
-        int anoNascimento = 1986;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o ano atual:");
+        int anoAtual = sc.nextInt();
+        System.out.println("Digite o ano do seu nascimento:");
+        int anoNascimento = sc.nextInt();
 
         int resultado = anoAtual - anoNascimento;
 
-        System.out.println("A idade é " + resultado);
+        System.out.println("A sua idade é " + resultado);
 
     }
 }

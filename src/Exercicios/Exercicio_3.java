4a8d0f6d4db82eaa51b66fbbee48bfd89d131f25
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-18 18:07:37 -0300

    add exercicio 3 media

---
+++ src/Exercicios/Exercicio_3.java
@@ -0,0 +1,22 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_3 {
+    public static void main(String[] args) {
+        // 3. Escreva um programa Java que calcule a média de três números inteiros.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero: ");
+        int num1 = sc.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int num2 = sc.nextInt();
+        System.out.println("Digite o terceiro numero: ");
+        int num3 = sc.nextInt();
+
+        int resultado = (num1 + num2 + num3) / 3;
+
+        System.out.println("O resultado da média dos 3 nmumeros é " + resultado);
+
+    }
+}

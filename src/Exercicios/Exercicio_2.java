673d5472f028c2edadf04216a27e9118c8a98e47
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-18 18:02:45 -0300

    add pack de exercicio e ex. 1

---
+++ src/Exercicios/Exercicio_2.java
@@ -0,0 +1,18 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_2 {
+    public static void main(String[] args) {
+        // 2. Escreva um programa que declare uma variável do tipo double e calcule
+        // o quadrado desse número.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número em cm: ");
+        double areaQuadrado = sc.nextDouble();
+        double perimetroQuadrado = areaQuadrado * 2;
+
+        System.out.println("o quadrado desse número é " + perimetroQuadrado);
+
+    }
+}

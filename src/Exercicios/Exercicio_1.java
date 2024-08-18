9832b1750b73f69a1125ef95c34571b265b0c131
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-18 17:57:14 -0300

    add pack de exercicio e ex. 1

---
+++ src/Exercicios/Exercicio_1.java
@@ -0,0 +1,17 @@
+package Exercicios;
+
+public class Exercicio_1 {
+    public static void main(String[] args) {
+
+        //1. Escreva um programa para declarar duas variáveis do tipo inteiro e calcular sua
+        //soma.
+
+        int num1 = 2;
+        int num2 = 8;
+
+        int resultado = num1 + num2;
+
+        System.out.println("Resultado: " + resultado);
+
+    }
+}

eef074fdf82c269fd4f8ca14416a7a7a58c51d05
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-18 18:18:55 -0300

    add exercicio 5

---
+++ src/Exercicios/Exercicio_5.java
@@ -0,0 +1,20 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_5 {
+
+    //5. Escreva um programa Java que converta graus Celsius para Fahrenheit.
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o numero d grau celsius: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println("O resultado da conversão para Fahrenheit é : " + fahrenheit + "ºF");
+
+        sc.close();
+    }
+}

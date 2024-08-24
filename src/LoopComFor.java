2faa53ba2cfa17490b1ae92b4ee7f2670d32ae78
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-23 22:49:26 -0300

    add melhoria no codigo loop for

---
+++ src/LoopComFor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LoopComFor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int valor = scanner.nextInt();
+
+        for (int contador = 1; contador <= valor; contador++) {
+            if (contador % 2 == 0) {
+                System.out.println(contador);
+            }
+        }
+        scanner.close();
+    }
+}

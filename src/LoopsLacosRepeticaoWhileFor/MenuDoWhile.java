6e385278512f4b7a60acf2bedc35bcce72bd2078
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-24 16:52:25 -0300

    add menu com do while else if

---
+++ src/LoopsLacosRepeticaoWhileFor/MenuDoWhile.java
@@ -0,0 +1,34 @@
+package LoopsLacosRepeticaoWhileFor;
+
+import java.util.Scanner;
+
+public class MenuDoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
+
+        do {
+            System.out.println("Digite uma opção do menu: ");
+            System.out.println("1. Sanduaiche");
+            System.out.println("2. Pizza");
+            System.out.println("3. Sorvete");
+            System.out.println("0. Sair");
+
+            opcao = scanner.nextInt();
+
+            if (opcao == 0) {
+                System.out.println("Saindo do sistema...");
+            } else if (opcao == 1) {
+                System.out.println("Você escolheu um sanduiche.");
+            } else if (opcao == 2) {
+                System.out.println("Você escolheu uma pizza.");
+            } else {
+                System.out.println("VocÊ escolheu um sorvete.");
+            }
+            //break;
+
+        } while (opcao != 0);
+
+        scanner.close();
+    }
+}

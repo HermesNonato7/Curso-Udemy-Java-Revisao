3055f7df093f2ed320ab51b378a7a020708369e9
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-24 22:42:13 -0300

    exercicio 3

---
+++ src/LoopsLacosRepeticaoWhileFor/Exercicos/Exercicio_3.java
@@ -0,0 +1,12 @@
+package LoopsLacosRepeticaoWhileFor.Exercicos;
+
+// 03. Escreva um programa Java que imprima os números pares de 2 a 20 usando um loop
+//for
+
+public class Exercicio_3 {
+    public static void main(String[] args) {
+        for (int numero = 2; numero <= 20; numero += 2) {
+            System.out.println(numero);
+        }
+    }
+}

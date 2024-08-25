d591dc9f9fcb4bcdcc51a2e23dd7b4f22a3e9d64
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-08-24 22:28:09 -0300

    exercicio 1

--- src/LoopsLacosRepeticaoWhileFor/Exercicos/Exercicio_1.java
+++ src/LoopsLacosRepeticaoWhileFor/Exercicos/Exercicio_1.java
@@ -2,7 +2,7 @@ package LoopsLacosRepeticaoWhileFor.Exercicos;
 
 // 1. Escreva um programa que imprima os números de 10 a 1 usando um loop while.
 
-public class NumerosDe10A1 {
+public class Exercicio_1 {
     public static void main(String[] args) {
         int i =  10;
 
